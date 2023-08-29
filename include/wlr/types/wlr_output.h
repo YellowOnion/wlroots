bfd920d39c1cc8743d58bdacf892980e2d34b12a
Daniel Hill <daniel@gluo.nz>
2023-08-29 23:03:35 +1200

    deferred cursors: add a max_cursor_latency for output

--- include/wlr/types/wlr_output.h
+++ include/wlr/types/wlr_output.h
@@ -161,6 +161,8 @@ struct wlr_output {
 	// true, changes may either succeed or fail.
 	bool adaptive_sync_supported;
 
+	int max_cursor_latency;
+
 	bool needs_frame;
 	// damage for cursors and fullscreen surface, in output-local coordinates
 	bool frame_pending;
